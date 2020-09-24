3544ab02dc7e39b16dd7bfac76da167ff1e76d5b
SardulDhyani <sarduldhyani07@gmail.com>
2020-09-24 13:30:25 +0530

    Updates

---
+++ precache-manifest.76ecad77a071f625789a61e9ec718fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4d90d03e4e2edfd6ab6",
+    "url": "/static/css/main.4f8a1c73.chunk.css"
+  },
+  {
+    "revision": "a4d90d03e4e2edfd6ab6",
+    "url": "/static/js/main.a4d90d03.chunk.js"
+  },
+  {
+    "revision": "820085f61afb483679bc",
+    "url": "/static/js/1.820085f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "775fb0747ac0105755efde29d1f530ce",
+    "url": "/index.html"
+  }
+];
